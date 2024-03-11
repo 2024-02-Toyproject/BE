a5327c2120b8cf8ffd6d3f5c071f239991d3cefc
Seohyeon.Lim <forestlight749@gmail.com>
2024-03-12 00:47:08 +0900

    CORS 설정 추가

---
+++ src/main/java/smu/toyproject1/WebConfig.java
@@ -0,0 +1,19 @@
+package smu.toyproject1;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    String ipAddress = "localhost";
+    String frontEndPort = "3000";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/test/ip")
+                .allowCredentials(true)
+                .allowedOrigins("http://"+this.ipAddress+":"+this.frontEndPort);
+    }
+}
